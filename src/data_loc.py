57d9a8462ec70e7a3b579404330dbd4155f78e88
Peter Le <105581900+petaa1@users.noreply.github.com>
2024-07-22 15:10:26 +0800

    data location file

---
+++ src/data_loc.py
@@ -0,0 +1,8 @@
+# This file holds the path to the directory containing geological surveys.
+
+# Instructions:
+# Replace the value of the 'directory_path' variable with the actual path to your directory.
+# For example, if your documents are located at "/path/to/geological_docs",
+# set -> directory_path = "/path/to/geological_docs"
+
+directory_path = "/geological_docs"  # Replace this with the location of your geological surveys
